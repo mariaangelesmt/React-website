1ffe43f6d01e0319b9cd8db4857cb5046152d2b9
mariaangelesmt <angymontesdeocatoribio@gmail.com>
2021-09-09 19:12:19 +0200

    did responsive to heroSection

---
+++ src/components/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'; 
+import HeroSection from '../HeroSection';
+import '../../App.css';
+
+
+function Home () {
+    return (
+        <>
+            <HeroSection />
+        </>
+    );
+}
+
+export default Home;
