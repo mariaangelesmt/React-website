2f06694f8bdaf2e384261d15ba524343a330cc12
mariaangelesmt <angymontesdeocatoribio@gmail.com>
2021-09-10 18:51:25 +0200

    added the cardItems and created the Products page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,14 +1,47 @@
-import React from 'react'
-import CardItem from './CardItem'
+import React from 'react';
+import CardItem from './CardItem';
+import './cards.css';
+
 
 function Cards() {
     return (
         <div className="cards">
             <h1>Check out these EPIC Destinations!</h1>
-            <div className="cards_container">
-                <div className="cards_wrapper">
-                    <ul className="cards_items">
-                        <CardItem />
+            <div className="cards__container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem 
+                            src="images/img9.png"
+                            text="Explore the hidden waterfall deep inside the Amazon Jungle"
+                            label="Adventure"
+                            path="/services"
+                        />
+                         <CardItem 
+                            src="images/img-2.jpg"
+                            text="Travel through the Islands of Bali in a Private Cruise"
+                            label="Luxury"
+                            path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem 
+                            src="images/img-3.jpg"
+                            text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
+                            label="Mystery"
+                            path="/services"
+                        />
+                        <CardItem 
+                            src="images/img-4.jpg"
+                            text="Experience Football on Top of the Himilayan Mountains"
+                            label="Adventure"
+                            path="/services"
+                        />
+                        <CardItem 
+                            src="images/img-8.jpg"
+                            text="Ride through the Sahara desert on a guided camel tour"
+                            label="Adrenaline"
+                            path="/services"
+                        />
                     </ul>
                 </div>
             </div>
